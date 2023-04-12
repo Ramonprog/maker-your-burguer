9351f209dc3182b848f9edeb659406bb819aa29a
ramonprog <programador.ramon@gmail.com>
2023-04-12 15:59:27 -0300

    backend integration

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -2,7 +2,32 @@
 export default {
   name: 'BurguerForm',
 
-  data() { }
+  data() {
+    return {
+      breadData: null,
+      meatData: null,
+      optionalData: null,
+      name: null,
+      bread: null,
+      meat: null,
+      optional: [],
+      status: 'Solicitado',
+      msg: null
+    }
+  },
+  methods: {
+    async getIngredients() {
+      const req = await fetch('http://localhost:3000/ingredientes')
+      const data = await req.json()
+
+      this.breadData = data.paes
+      this.meatData = data.carnes
+      this.optionalData = data.opcionais
+    }
+  },
+  mounted() {
+    this.getIngredients()
+  }
 }
 </script>
 
@@ -21,7 +46,9 @@ export default {
           <label for="bread">Escolha o pão: </label>
           <select name="bread" id="bread" v-model="bread">
             <option value="">Selecione o seu pão</option>
-            <option value="integral">Integral</option>
+            <option :value="bread.tipo" v-for="bread in breadData" :key="bread.id">
+              {{ bread.tipo }}
+            </option>
           </select>
         </div>
 
@@ -29,42 +56,18 @@ export default {
           <label for="meat">Escolha a carne do seu Burger </label>
           <select name="meat" id="meat" v-model="meat">
             <option value="">Selecione o tipo de carne</option>
-            <option value="maminha">Maminha</option>
+            <option v-for="meat in meatData" :value="meat" :key="meat.id">{{ meat.tipo }}</option>
           </select>
         </div>
 
         <div class="input-container">
           <label for="optional" id="optional-title">Selecione os opcionais: </label>
-          <div class="checkbox">
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
-            <div class="checkbox-cointainer">
-              <input type="checkbox" name="optional" id="optional" v-model="optional" value="salame" />
-              <span>Salame</span>
-            </div>
 
+          <div class="checkbox">
+            <div class="checkbox-cointainer" v-for="optionalItem in optionalData" :key="optionalItem.id">
+              <input type="checkbox" name="optional" id="optional" v-model="optional" />
+              <span>{{ optionalItem.tipo }}</span>
+            </div>
           </div>
         </div>
 
@@ -75,8 +78,6 @@ export default {
     </div>
   </div>
 </template>
-
-
 
 <style scoped>
 form {
@@ -117,18 +118,20 @@ select {
   width: 100%;
   display: flex;
   flex-wrap: wrap;
-
+  max-width: 300px;
+  justify-content: space-between;
 }
 
 .checkbox-cointainer {
   display: flex;
   flex-wrap: wrap;
+
 }
 
 .checkbox-cointainer input {
   width: auto;
-  margin-right: 10px;
-  margin-bottom: 20px;
+  margin-right: 5px;
+  margin-bottom: 15px;
 }
 
 .checkbox-cointainer span {
@@ -145,7 +148,7 @@ select {
   padding: 10px;
   font-size: 16px;
   cursor: pointer;
-  transition: .5s;
+  transition: 0.5s;
 }
 
 .submit-btn:hover {
